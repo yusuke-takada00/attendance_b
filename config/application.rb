f981483b3ed7c717b7b2bbc00ff2e08fd5196c89
yusuke-takada00 <yu.neiho@gmail.com>
2020-11-22 15:46:50 +0000

    topページ追加

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module AttendanceB
   class Application < Rails::Application
+    config.load_defaults 5.1
+    config.time_zone = 'Asia/Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
